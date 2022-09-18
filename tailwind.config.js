00b21864d39caafc7893eab319f17c8455a33f83
matheoepie <matheoepie@outlook.com>
2022-09-18 17:30:20 +0200

    Add tailwindcss config files

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#2563eb",
+
+          "secondary": "#D926A9",
+
+          "accent": "#1FB2A6",
+
+          "neutral": "#191D24",
+
+          "base-100": "#2A303C",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
